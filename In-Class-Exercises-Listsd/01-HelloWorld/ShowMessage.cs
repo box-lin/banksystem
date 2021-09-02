94d85b1cff93d783713942557a3cb5d3f46a222c
Boxiang Lin <boxiang.lin@wsu.edu>
2021-09-01 19:24:02 -0700

    Print and Write HelloWorld function added

---
+++ In-Class-Exercises-Listsd/01-HelloWorld/ShowMessage.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace _01_HelloWorld
+{
+    public class ShowMessage
+    {
+        private string message;
+        
+        public ShowMessage()
+        {
+            this.message = null;
+        }
+
+        public ShowMessage(string message)
+        {
+            this.message = message;
+            showMessage(message);
+        }
+
+        public void showMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public void writeMessage(string message)
+        {
+            string path = "C:\\Users\\boxiang\\Desktop\\Fall2021\\CPTS321\\cpts321-in-class-exercises\\In-Class-Exercises-Listsd";
+            FileStream fs = new FileStream(path + "\\01-HelloWorld.txt", FileMode.Create);
+            StreamWriter sw = new StreamWriter(fs);
+            sw.Write(message);
+            Console.WriteLine("Please check the file at this directory: " + path);
+            sw.Flush();
+            sw.Close();
+            fs.Close();
+
+        }
+    }
+}
---
+++ In-Class-Exercises-Listsd/01-HelloWorld/SingleLinkedList.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01_HelloWorld
+{
+    class SingleLinkedList
+    {
+    }
+}
