8669ac1c8f50545f50397adbb2ecca4d4a934190
Boxiang Lin <boxiang.lin@wsu.edu>
2021-09-01 17:00:42 -0700

    part work for stdin

---
+++ InClassExercise/InClassExercise/Angle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InClassExercise
+{
+    class Angle
+    {
+        private double angleRadians;
+        public double AngleDegrees
+        {
+            get { return angleRadians * 180.0 / Math.PI; }
+            set { angleRadians = value / 180.0 * Math.PI; }
+        }
+    }
+}
---
+++ InClassExercise/InClassExercise/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace InClassExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            while (true)
+            {
+                Console.WriteLine("Please enter the commands below: ");
+                Console.WriteLine("1 = Show the pass by reference and pass by value scenario ");
+                Console.WriteLine("2 = Show Hello World on the screen ");
+                Console.WriteLine("3 = Write Hello in a file ");
+                Console.WriteLine("4 = Adding two numbers in a Linked List");
+                Console.WriteLine("0 = Quit");
+
+                string commands = Console.ReadLine();
+                run(commands);
+            }
+        }
+        //Constructor
+        public Program()
+        {
+            
+        }
+
+        private static void run(string c)
+        {
+            if (c == null){
+                Console.WriteLine("No Command Founded!");
+            }
+            switch (c)
+            {
+                case "0":
+                    Console.WriteLine("quit");
+                    break;
+                case "1":
+                    Console.WriteLine("case 1！");
+                    break;
+                case "2":
+                    Console.WriteLine("case 2！");
+                    break;
+                case "3":
+                    Console.WriteLine("case 3！");
+                    break;
+                case "4":
+                    Console.WriteLine("case 4！");
+                    break;
+
+            }
+               
+
+        }
+    }
+}
