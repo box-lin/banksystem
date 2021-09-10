93b3b89d243136ee34637c1e7e4db68c686122b9
Boxiang Lin <boxiang.lin@wsu.edu>
2021-09-10 12:04:56 -0700

    use Nunit to test the add function in Math class, since the add function not been implemented, the test result shows the test cases were failed

---
+++ 02-HelloWorldTests/HelloWorld/Math.cs
@@ -0,0 +1,7 @@
+﻿namespace HelloWorld
+{
+    public class Math
+    {
+        public static int Add(int a, int b) { return 0;  }
+    }
+}
---
+++ 02-HelloWorldTests/HelloWorldTests/TestMath.cs
@@ -0,0 +1,25 @@
+﻿// NUnit 3 tests
+// See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using HelloWorld;
+
+namespace HelloWorldTests
+{
+    [TestFixture]
+    public class TestMath
+    {   
+        [Test]
+        public void TestAdd()
+        {
+
+            // TODO: Add your test code here
+            Assert.AreEqual(5, Math.Add(2, 3));
+            Assert.AreEqual(5, Math.Add(1, 4));
+            Assert.AreEqual(-5, Math.Add(0, -5));
+            Assert.AreEqual(10, Math.Add(-10, 20));
+
+        }
+    }
+}
