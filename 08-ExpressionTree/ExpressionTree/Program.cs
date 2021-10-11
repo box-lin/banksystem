84c4bfb92269635d6f84d1e576ff7ea699f6217f
Boxiang Lin <boxiang.lin@wsu.edu>
2021-10-11 14:25:18 -0700

    tested the ExpressionTree found that it doesnt work for negative values

---
+++ 08-ExpressionTree/ExpressionTree/Program.cs
@@ -0,0 +1,25 @@
+﻿// CptS 321: Expression Tree Code Demo of how NOT to code your assignements
+// Problems and sollutions of this code will be discussed in class
+// Note that if you sumbit this code you will not get ANY points for the assignments
+
+using System;
+
+namespace ExpressionTreeCodeDemo
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+
+            while (true)
+            {
+                Console.Write("Enter expression: ");
+                string line = Console.ReadLine();
+                Expression exp = new Expression(line);
+                Console.WriteLine(exp.Evaluate().ToString());
+
+                Console.ReadKey();
+            }
+		}
+	}
+}
---
+++ 08-ExpressionTree/ExpressionTreeTest/TestExpressionTree.cs
@@ -0,0 +1,90 @@
+﻿// NUnit 3 tests
+// See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
+using NUnit.Framework;
+using ExpressionTreeCodeDemo;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ExpressionTreeTest
+{
+    [TestFixture]
+    public class TestExpressionTree
+    {
+        Expression exp;
+
+        [Test]
+        public void TestAdd()
+        {
+            exp = new Expression("5+3");
+            Assert.AreEqual("8", exp.Evaluate().ToString());
+
+            exp = new Expression("(-3)+(-5)");
+            Assert.AreEqual("-8", exp.Evaluate().ToString());
+
+        }
+
+        [Test]
+        public void TestSubtract()
+        {
+            exp = new Expression("5-3");
+            Assert.AreEqual("2", exp.Evaluate().ToString());
+
+            exp = new Expression("(-3)-(-5)");
+            Assert.AreEqual("2", exp.Evaluate().ToString());
+
+        }
+
+        [Test]
+        public void TestMulitply()
+        {
+            exp = new Expression("5*3");
+            Assert.AreEqual("15", exp.Evaluate().ToString());
+
+            exp = new Expression("(-3)*(-5)");
+            Assert.AreEqual("15", exp.Evaluate().ToString());
+
+        }
+
+        [Test]
+        public void TestDivide()
+        {
+            exp = new Expression("10/2");
+            Assert.AreEqual("5", exp.Evaluate().ToString());
+
+            exp = new Expression("5/2");
+            Assert.AreEqual("2.5", exp.Evaluate().ToString());
+
+            exp = new Expression("(-5)/2");
+            Assert.AreEqual("-2.5", exp.Evaluate().ToString());
+
+        }
+
+        [Test]
+        public void TestExponent()
+        {
+            exp = new Expression("10^2");
+            Assert.AreEqual("100", exp.Evaluate().ToString());
+
+            exp = new Expression("(-5)^2");
+            Assert.AreEqual("25", exp.Evaluate().ToString());
+
+        }
+
+
+        [Test]
+        public void TestNestedParenthesis()
+        {
+            exp = new Expression("(((5)))+2");
+            Assert.AreEqual("7", exp.Evaluate().ToString());
+
+            exp = new Expression("(10)-((((2))))");
+            Assert.AreEqual("8", exp.Evaluate().ToString());
+
+        }
+
+
+
+
+
+    }
+}
