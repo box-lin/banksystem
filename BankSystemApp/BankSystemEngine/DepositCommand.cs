c5413159f2e1ccbb997e044d8fbce5b492db074b
Boxiang Lin <57877290+boxianglin@users.noreply.github.com>
2021-12-08 12:38:38 -0800

    add skeleton code for Deposite/Withdraw command class

---
+++ BankSystemApp/BankSystemEngine/DepositCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankSystemEngine
+{
+    /// <summary>
+    /// Deposite command to do the deposit.
+    /// </summary>
+    public class DepositCommand : ICommand
+    {
+        /// <summary>
+        /// Execute the deposite.
+        /// </summary>
+        /// <returns> can execute or not. </returns>
+        public bool Execute()
+        {
+            return false;
+        }
+
+        /// <summary>
+        /// Unexecute the deposite.
+        /// </summary>
+        public void Unexecute()
+        {
+        }
+    }
+}
---
+++ BankSystemApp/BankSystemEngine/WithdrawCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankSystemEngine
+{
+    /// <summary>
+    /// Withdraw command class to handle the withdraw.
+    /// </summary>
+    public class WithdrawCommand : ICommand
+    {
+
+        /// <summary>
+        /// Execute the withdraw.
+        /// </summary>
+        /// <returns> can withdraw or not. </returns>
+        public bool Execute()
+        {
+            return false;
+        }
+
+        /// <summary>
+        /// Unexecute the withdraw.
+        /// </summary>
+        public void Unexecute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
