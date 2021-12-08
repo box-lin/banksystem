7dad733041ee0ea6071632d25fea20351672def2
Boxiang Lin <57877290+boxianglin@users.noreply.github.com>
2021-12-08 12:29:31 -0800

    TDD test further for the SavingAccount.

--- BankSystemApp/BankSystemEngineTest/TestSavingAccount.cs
+++ BankSystemApp/BankSystemEngineTest/TestSavingAccount.cs
@@ -1,20 +1,101 @@
-﻿// NUnit 3 tests
-// See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
+﻿// <copyright file="TestSavingAccount.cs" company="Boxiang Lin - WSU 011601661">
+// Copyright (c) Boxiang Lin - WSU 011601661. All rights reserved.
+// </copyright>
+
+using BankSystemEngine;
 using NUnit.Framework;
-using System.Collections;
-using System.Collections.Generic;
 
 namespace BankSystemEngineTest
 {
+    /// <summary>
+    /// Test saving account.
+    /// </summary>
     [TestFixture]
     public class TestSavingAccount
     {
+
+        /// <summary>
+        /// Test get account number.
+        /// </summary>
         [Test]
-        public void TestMethod()
+        public void TestGetAccountNumber()
         {
-            // TODO: Add your test code here
-            var answer = 42;
-            Assert.That(answer, Is.EqualTo(42), "Some useful error message");
+            /// <summary>
+            /// saving account instance.
+            /// </summary>
+            SavingAccount checkAcc = new SavingAccount(123);
+            Assert.AreEqual(checkAcc.GetAccNumber(), 123);
+        }
+
+        /// <summary>
+        /// Test set account number.
+        /// </summary>
+        [Test]
+        public void TestSetAccountNumber()
+        {
+            /// <summary>
+            /// saving account instance.
+            /// </summary>
+            SavingAccount checkAcc = new SavingAccount(123);
+            checkAcc.SetAccNumber(321);
+            Assert.AreEqual(checkAcc.GetAccNumber(), 321);
+        }
+
+        /// <summary>
+        /// Test get account balance.
+        /// </summary>
+        [Test]
+        public void TestGetAccountBalance()
+        {
+            /// <summary>
+            /// saving account instance.
+            /// </summary>
+            SavingAccount checkAcc = new SavingAccount(123);
+            Assert.AreEqual(0.0, checkAcc.GetAccBalance());
+
+        }
+
+        /// <summary>
+        /// Test deposit to balance..
+        /// </summary>
+        [Test]
+        public void TestDeposit()
+        {
+            /// <summary>
+            /// saving account instance.
+            /// </summary>
+            SavingAccount checkAcc = new SavingAccount(123);
+            checkAcc.Deposit(100.0);
+            Assert.AreEqual(100.0, checkAcc.GetAccBalance());
+        }
+
+        /// <summary>
+        /// Test balance withdraw.
+        /// </summary>
+        [Test]
+        public void TestWithdrawFail()
+        {
+            /// <summary>
+            /// saving account instance.
+            /// </summary>
+            SavingAccount checkAcc = new SavingAccount(123);
+            bool isSucess = checkAcc.Withdraw(100.0);
+            Assert.AreEqual(false, isSucess);
+        }
+
+        /// <summary>
+        /// Test balance withdraw.
+        /// </summary>
+        [Test]
+        public void TestWithdrawSucess()
+        {
+            /// <summary>
+            /// saving account instance.
+            /// </summary>
+            SavingAccount checkAcc = new SavingAccount(123);
+            checkAcc.Deposit(500.0);
+            bool isSucess = checkAcc.Withdraw(100.0);
+            Assert.AreEqual(true, isSucess);
         }
     }
 }
