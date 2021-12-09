0ed5b1ff21dae9357aedb94983d4e3af21d81817
Boxiang Lin <57877290+boxianglin@users.noreply.github.com>
2021-12-08 19:58:30 -0800

    added a generic user class with getter and setter setup

--- BankSystemApp/BankSystemEngine/ITransaction.cs
+++ BankSystemApp/BankSystemEngine/ITransaction.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿// <copyright file="ITransaction.cs" company="Boxiang Lin - WSU 011601661">
+// Copyright (c) Boxiang Lin - WSU 011601661. All rights reserved.
+// </copyright>
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
